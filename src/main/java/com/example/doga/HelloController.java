e8885587ba7b410ab905396352780d9dea296244
YoSlurP <cseh.lalika.gergo@gmail.com>
2024-03-12 13:07:25 +0100

    Timer

--- src/main/java/com/example/doga/HelloController.java
+++ src/main/java/com/example/doga/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.doga;
 
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
@@ -9,14 +11,30 @@ public class HelloController {
     @FXML private Label lbopen;
     @FXML private Pane pnJatek;
     @FXML private Label lbclose;
+    @FXML private ImageView nyil;
 
-    private ImageView[][] tomb= new ImageView[10][15];
+    private Label[][] tomb= new Label[10][15];
+    private AnimationTimer timer =null;
 
     public void initialize(){
         for(int s=0;s<10;s++){
             for(int o=0;o<15;o++){
-
+                tomb[s][o]= new Label();
+                tomb[s][o].setGraphic(new ImageView(new Image(getClass().getResourceAsStream("box.png"))));
+                tomb[s][o].setTranslateX(10+o*64);
+                tomb[s][o].setTranslateY(10+s*64);
+                int ss=s,oo=o;
+                tomb[s][o].setOnMouseEntered(e -> tomb[ss][oo].setStyle("-fx-background-color: lightgreen;"));
+                tomb[s][o].setOnMouseExited(e -> tomb[ss][oo].setStyle("-fx-background-color: white;"));
+                pnJatek.getChildren().add(tomb[s][o]);
             }
         }
+        timer = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                nyil.setRotate(nyil.getRotate()+6);
+            }
+        };
+        timer.start();
     }
 }
